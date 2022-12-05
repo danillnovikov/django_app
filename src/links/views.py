16cc4db19850f251b2e6bbcfdeef3a12c1358ea3
Daniil <danill.novikov@mail.ru>
2022-12-05 14:34:13 +0200

    add views and template

--- src/links/views.py
+++ src/links/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+from links.models import Links
 # Create your views here.
+
+
+
+def home_page(request):
+    links = Links.objects.all()
+    context = {"links": links}
+    return render(request, template_name="home.html", context=context)
