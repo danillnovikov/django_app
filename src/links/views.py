fe9b4e955652d0cfad70b27019c113ba22ba4f04
Daniil <danill.novikov@mail.ru>
2022-12-06 17:04:50 +0200

    add templates, links, work with views

--- src/links/views.py
+++ src/links/views.py
@@ -1,12 +1,36 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 from links.models import Links
 # Create your views here.
 
 
 
-def home_page(request):
+def links_list(request):
     links = Links.objects.all()
     context = {"links": links}
     return render(request, template_name="home.html", context=context)
+
+
+def links_detail(request, pk):
+    link = Links.objects.get(pk=pk)
+    context = {"object": link}
+    return render(request, template_name="detail.html", context=context)
+
+def links_delete(request, pk):
+    link = Links.objects.get(pk=pk)
+    message = f'Videos {link.name} has just deleted!!!'
+    link.delete()
+    context = {"message": message}
+    return render(request, template_name="delete.html", context=context)
+
+def links_create(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        links = request.POST.get('links')
+        link = Links.objects.create(name=name, links=links)
+        return HttpResponseRedirect(reverse('videos-detail', kwargs={'pk': link.pk}))
+    context = {}
+    return render(request, template_name="create.html", context=context)
+
--- src/proj/urls.py
+++ src/proj/urls.py
@@ -16,9 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from links.views import home_page
+from links import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', home_page)
+    path('admin/', admin.site.urls),   # http://127.0.0.1:8000/admin/
+    path('links/', views.links_list, name="videos-list"),    # http://127.0.0.1:8000/links/
+    path('links/<int:pk>/', views.links_detail, name="videos-detail"), # http://127.0.0.1:8000/links/
+    path('links-delete/<int:pk>/', views.links_delete, name="videos-delete"), # http://127.0.0.1:8000/links/
+    path('links-create/', views.links_create, name="videos-create"), # http://127.0.0.1:8000/links/
+    # 1. http://127.0.0.1:8000/links/*/            (URL)
+    # 2. http://127.0.0.1:8000/links/?links_id=4 (Get)
+    # 3. key:value, key:value                     (POST)
 ]
